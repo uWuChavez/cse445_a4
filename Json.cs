a10dab4296d111f6b7ea841177785598fb5472cb
uWuChavez <123106503+uWuChavez@users.noreply.github.com>
2025-11-02 13:30:39 -0700

    Add files via upload

---
+++ Json.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ConsoleApp1
+{
+    public static class Xml2Json
+    { 
+        public static string Xml2Json(string xmlUrl)
+        {
+            // Convert the XML file located at xmlUrl to a JSON string and return it.
+            try
+            {
+                XDocument xmlDoc = XDocument.Load(xmlUrl);
+                string jsonText = JsonConvert.SerializeXmlNode(xmlDoc, Formatting.Indented);
+                return jsonText;
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+    }
+}
---
+++ Validate.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+using System.Xml;
+using System.Xml.Schema;
+
+namespace ConsoleApp1
+{
+    public static class Validate
+    {
+        public static string Verification(string xmlUrl, string xsdUrl)
+        {
+            StringBuilder errors = new StringBuilder();
+            XmlReaderSettings settings = new XmlReaderSettings();
+            settings.Schemas.Add(null, xsdUrl);
+            settings.ValidationType = ValidationType.Schema;
+            settings.ValidationEventHandler += (sender, e) =>
+            {
+                errors.AppendLine($"Line {e.Exception.LineNumber}: {e.Message}");
+            };
+
+            using (XmlReader reader = XmlReader.Create(xmlUrl, settings))
+            {
+                try
+                {
+                    while (reader.Read()) { }
+                }
+                catch (Exception ex)
+                {
+                    errors.AppendLine(ex.Message);
+                }
+            }
+            return errors.Length == 0 ? "No Error" : errors.ToString();
+        }
+    }
+}
